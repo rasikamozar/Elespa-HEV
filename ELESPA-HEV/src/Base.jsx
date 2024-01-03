aa79469c8a78234cb4e82a82d5bf51f6e57ff0c0
Rasika Vikas Mozar <rasikamozar@gmail.com>
2024-01-03 11:26:29 +0530

    Your commit message

--- ELESPA-HEV/src/Base.jsx
+++ ELESPA-HEV/src/Base.jsx
@@ -1,10 +1,12 @@
 import './Base.css'
+import ReactPlayer from 'react-player'
+import video from './Database/demo.mp4'
 
 export default function Base(){
     return(
           <div >
               <div className="Box">
-                      <h1 className='Heading'>Electrifying India </h1>
+                      <ReactPlayer url={video} controls={true}  width={1530} height={700} playing={true}/>
               </div>
           </div>
     );
